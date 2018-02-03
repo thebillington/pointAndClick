09ea6d22a99fb745ac62b1d517418b7cf08b1a4a
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2018-02-03 15:08:35 +0000

    Setup sprites

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 // Create the player variable
-var player;
+var playerRect;
+
+// Get the sprite images
+var playerImg;
+var pickupImg;
+
+// Pre load function
+function preload() {
+    
+    // Load our sprite images
+    playerImg = loadImage("player.png");
+    
+}
 
 // Create the setup function
 function setup() {
@@ -8,7 +20,7 @@ function setup() {
     createCanvas(600, 600);
     
     // Create the player rectangle
-    player = Rectangle(300, 300, color(50, 255, 255));
+    playerRect = imgGetRect(playerImg);
     
 }
 
@@ -19,28 +31,28 @@ function draw() {
     clear();
     
     // Draw a rectangle
-    drawRect(player);
+    drawSprite(playerImg, playerRect);
     
     // Check key presses
     if(keyIsDown(LEFT_ARROW)) {
         
         //Move the player
-        player.x -= 1;
+        playerRect.x -= 1;
     }
     if(keyIsDown(RIGHT_ARROW)) {
         
         //Move the player
-        player.x += 1;
+        playerRect.x += 1;
     }
     if(keyIsDown(UP_ARROW)) {
         
         //Move the player
-        player.y -= 1;
+        playerRect.y -= 1;
     }
     if(keyIsDown(DOWN_ARROW)) {
         
         //Move the player
-        player.y += 1;
+        playerRect.y += 1;
     }
     
 }
@@ -52,14 +64,30 @@ function drawRect(rect) {
     fill(rect.colour);
     
     // Draw a rectangle
-    quad( rect.x-50,rect.y-50, rect.x+50,rect.y-50, rect.x+50,rect.y+50, rect.x-50,rect.y+50);
+    quad( rect.x,rect.y, rect.x+rect.width,rect.y, rect.x+rect.width,rect.y+rect.height, rect.x,rect.y+rect.height);
+    
+}
+
+// Function to draw a sprite
+function drawSprite(img, rect) {
+    
+    // Draw the image at the rect location
+    image(img, rect.x, rect.y, rect.width, rect.height);
     
 }
 
 // Create a function to return a rectangle object
-function Rectangle(_x, _y, _colour) {
+function Rectangle(_x, _y, _width, _height, _colour) {
     
     // Return a rectangle object
-    return {x: _x, y: _y, colour: _colour};
+    return {x: _x, y: _y, width: _width, height: _height, colour: _colour};
+    
+}
+
+// Create a function to get a rectangle from an image
+function imgGetRect(img) {
+    
+    // Return a rectangle at 0,0 with width and height of image
+    return Rectangle(0, 0, img.width, img.height, -1);
     
 }
